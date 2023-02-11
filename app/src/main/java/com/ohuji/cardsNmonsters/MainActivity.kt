f905bc2c889a51832d18e908c6304f77c5576bdc
ohuji <juhsalomaki@gmail.com>
2023-02-11 21:13:25 +0200

    Added navigationHost and navigation depedency

--- app/src/main/java/com/ohuji/cardsNmonsters/MainActivity.kt
+++ app/src/main/java/com/ohuji/cardsNmonsters/MainActivity.kt
@@ -6,12 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ohuji.cardsNmonsters.screens.augmented_reality.ARScreen
 import com.ohuji.cardsNmonsters.ui.theme.CardsMonstersTheme
+import com.ohuji.cardsNmonsters.utils.NavigationHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ARScreen()
+                    NavigationHost()
                 }
             }
         }
---
+++ app/src/main/java/com/ohuji/cardsNmonsters/utils/Navigation.kt
@@ -0,0 +1,33 @@
+package com.ohuji.cardsNmonsters.utils
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ohuji.cardsNmonsters.screens.augmented_reality.ARScreen
+
+@Composable
+fun NavigationHost() {
+    val navController = rememberNavController()
+
+    NavHost(navController, startDestination = "ar_screen") {
+        composable("ar_screen") {
+            ARScreen(navController = navController)
+        }
+        composable("home_screen") {
+
+        }
+        composable("map_screen") {
+
+        }
+        composable("deck_building") {
+
+        }
+        composable("collectables_screen") {
+
+        }
+        composable("collectable_screen") {
+
+        }
+    }
+}
